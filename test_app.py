2bc2cb1a9d9b043b453dbf4b8d2ffc2b02e67b77
Dineshrepo6746 <dineshdp6746@gmail.com>
2025-06-30 08:19:15 +0000

    error

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
         self.assertTrue(is_palindrome("Racecar"))
         self.assertTrue(is_palindrome("A man, a plan, a canal: Panama"))
         self.assertFalse(is_palindrome("hello"))
-        self.assertFalse(is_palindrome("Python"))
+        self.assertFalse(is_palindrome("Python
 
 if __name__ == '__main__':
     unittest.main()
